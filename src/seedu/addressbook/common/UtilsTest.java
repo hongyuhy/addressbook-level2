823b25e3cbbd6ff8f19aa7172fdf3db5dd920aec
Tan Hong Yu <TanHongYu@Hy-2.local>
2016-09-04 15:18:25 +0800

    changed name of method for better readability

--- src/seedu/addressbook/common/UtilsTest.java
+++ src/seedu/addressbook/common/UtilsTest.java
@@ -9,21 +9,21 @@ import org.junit.Test;
 public class UtilsTest {
 
 	@Test
-	public void isAnyNullTest() {
+	public void isAnyNullTest_noNull_returnFalse() {
 		String[] listOfString = {"hi","hello","heehee"};
 		boolean result = Utils.isAnyNull(listOfString);
 		assertEquals(false, result);
 	}
 	
 	@Test
-	public void isAnyNullTest2(){
+	public void isAnyNullTest_nullInObject_returnTrue(){
 		String[] listOfString = {null};
 		boolean result = Utils.isAnyNull(listOfString);
 		assertEquals(true, result);
 	}
 	
 	@Test
-	public void elementsAreUniqueTest(){
+	public void elementsAreUniqueTest_allUnique_returnTrue(){
 		ArrayList<String> arrayListOf = new ArrayList<String>();
 		arrayListOf.add("hi");
 		arrayListOf.add("hello");
@@ -32,7 +32,7 @@ public class UtilsTest {
 	}
 
 	@Test
-	public void elementsAreUniqueTest2(){
+	public void elementsAreUniqueTest_notUnique_returnFalse(){
 		ArrayList<String> arrayListOf = new ArrayList<String>();
 		arrayListOf.add("hi");
 		arrayListOf.add("hi");
